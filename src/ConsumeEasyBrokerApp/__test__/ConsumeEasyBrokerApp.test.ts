5722670d44c2e6c1497e07473cc8b4e334becf50
Ramiro Nicolas D'Accorso Rosati <rami@MacBook-Pro-de-Ramiro.local>
2023-09-13 09:36:49 -0300

    fix: fix test to be unit test only

--- src/ConsumeEasyBrokerApp/__test__/ConsumeEasyBrokerApp.test.ts
+++ src/ConsumeEasyBrokerApp/__test__/ConsumeEasyBrokerApp.test.ts
@@ -1,13 +1,15 @@
 import { ConsumeEasyBrokerApp } from "../ConsumeEasyBrokerApp";
 import { EXPECTED_CONTENT } from "../__mock__/EXPECTED_CONTENT";
+import { Property } from "../../Entity/Property";
 
 describe("ConsumeEasyBrokerApp should", () => {
   test("call paginated api should return properties ", async () => {
     process.env.TEST_API_KEY = "l7u502p8v46ba3ppgvj5y2aad50lb9";
     const c = new ConsumeEasyBrokerApp();
-    const page = 1;
-    const limit = 20;
-    const res = await c.getProperties(page, limit);
+    const propertyMock: Property[] = EXPECTED_CONTENT;
+    c.getAllProperties = jest.fn().mockReturnValue(propertyMock);
+
+    const res: Property[] = await c.getAllProperties();
     expect(res).toEqual(EXPECTED_CONTENT);
   });
 });
--- src/EasyBrokerAppService/__test__/EasyBrokerApp.test.ts
+++ src/EasyBrokerAppService/__test__/EasyBrokerApp.test.ts
@@ -1,30 +1,32 @@
-import { EasyBrokerApp, GetAllPropertiesProps } from "../EasyBrokerApp";
+import { EasyBrokerApp } from "../EasyBrokerApp";
 
 const TEST_API_KEY: string = "l7u502p8v46ba3ppgvj5y2aad50lb9";
-const GET_ALL_PROPERTIES_PROPS: GetAllPropertiesProps = {
-  limit: "20",
-  page: "1",
-};
+
+jest.mock("api", () => () => ({
+  auth: jest.fn(),
+  getProperties: jest.fn(),
+}));
 
 describe("EasyBrokerAppService should", () => {
   it("be defined", async () => {
-    const app = new EasyBrokerApp(TEST_API_KEY);
+    const sdk = require("api")("@easybroker-staging/v1.0#32afigk2plm5b3e8d");
+    const app = new EasyBrokerApp(sdk, TEST_API_KEY);
     expect(app).toBeDefined();
-    expect(
-      await app.getAllProperties({} as GetAllPropertiesProps),
-    ).toBeDefined();
-  });
-
-  it("should return all properties paginated", async () => {
-    const app = new EasyBrokerApp(TEST_API_KEY);
-    const res = await app.getAllProperties(GET_ALL_PROPERTIES_PROPS);
-    expect(res).toBeDefined();
   });
 
   it("should return error message if api Key is not correct", async () => {
     try {
-      const app = new EasyBrokerApp("WRONG_TEST_API_KEY");
-      const res = await app.getAllProperties(GET_ALL_PROPERTIES_PROPS);
+      const sdk = require("api")("@easybroker-staging/v1.0#32afigk2plm5b3e8d");
+      sdk.getProperties = jest.fn(() =>
+        Promise.reject({
+          data: {
+            error: "Your API key is invalid",
+          },
+        }),
+      );
+      const app = new EasyBrokerApp(sdk, "WRONG_TEST_API_KEY");
+
+      await app.getAllProperties();
     } catch (e) {
       expect(e.message).toBe("Your API key is invalid");
     }
